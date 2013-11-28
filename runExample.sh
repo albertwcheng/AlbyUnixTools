8b60d7e79befd6a602b2e97fd2b56c060dd8a734
albertwcheng <albertwcheng@gmail.com>
2013-11-28 00:22:28 -0500

    commit

---
+++ runExample.sh
@@ -0,0 +1,1 @@
+./joina -w 2 --cols .GeneName --add-prefix geneInfo. --of-file examples/example2.txt --add-prefix Expression. --of-file examples/example1.txt --add-prefix extref. --of-file examples/example3.txt 
